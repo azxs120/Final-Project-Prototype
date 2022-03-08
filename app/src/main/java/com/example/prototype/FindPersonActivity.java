764f09d62bf9742b3e2522b0a7481dafa36ed4cd
azxs120 <hazi120@walla.com>
2022-03-08 15:55:10 +0200

    גילינו שיש בעיה בהצגת searchView

--- app/src/main/java/com/example/prototype/FindPersonActivity.java
+++ app/src/main/java/com/example/prototype/FindPersonActivity.java
@@ -3,43 +3,51 @@ package com.example.prototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+
+
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class FindPersonActivity extends AppCompatActivity {
+
     ListView listView;
-    ArrayList<String> stringArrayList= new ArrayList<>();
+    ArrayList<String> stringArrayList = new ArrayList<>();
     ArrayAdapter<String> adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_person);
+
+
         this.setTitle("חפש");
 
-        listView = findViewById(R.id.searchView);
+        //listView = findViewById(R.id.searchView);
         //add item in array list
-        for (int i=0; i<=100; i++){
-            stringArrayList.add("Item" +i);
-        }
+        for (int i = 0; i <= 100; i++)
+            stringArrayList.add("Item" + i);
+
+        /*
         //Imitialize adapter
         adapter = new ArrayAdapter<>(FindPersonActivity.this
-                ,android.R.layout.simple_list_item_1,stringArrayList);
+                , android.R.layout.simple_list_item_1, stringArrayList);
         //Set adapter on list view
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,int postion, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Display click item position in toast
-                Toast.makeText(getApplicationContext(), adapter.getItem(postion),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), adapter.getItem(position), Toast.LENGTH_SHORT).show();
             }
 
         });
-    }
-}
 
+ */
+    }
+
+
+}
