1ba1a2070d2b163d501e4992b934cdad75f2912b
azxs120 <hazi120@walla.com>
2022-04-12 18:16:11 +0300

    made the login work wile empty
    added a condition that checks if the email and password are empty and if so the app will go to main
    
    Login row 66

--- app/src/main/java/com/example/prototype/LoginActivity.java
+++ app/src/main/java/com/example/prototype/LoginActivity.java
@@ -63,32 +63,33 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //DB code
-                Credentials credentials = Credentials.emailPassword(emailEditText.getText().toString(), passwordEditText.getText().toString());
-                app.loginAsync(credentials, new App.Callback<User>() {
-                    @Override
-                    public void onResult(App.Result<User> result) {
+                if(!(emailEditText.getText().toString().isEmpty() ) && (!(passwordEditText.getText().toString()).isEmpty()))
+                {
+                    Credentials credentials = Credentials.emailPassword(emailEditText.getText().toString(), passwordEditText.getText().toString());
+                    app.loginAsync(credentials, new App.Callback<User>() {
+                        @Override
+                        public void onResult(App.Result<User> result) {
 
-                    }
-                });
+                        }
+                    });
 
-                User user = app.currentUser();
-                mongoClient = user.getMongoClient("mongodb-atlas");
-                mongoDatabase = mongoClient.getDatabase("RentMe");//the cluster(project)
+                    User user = app.currentUser();
+                    mongoClient = user.getMongoClient("mongodb-atlas");
+                    mongoDatabase = mongoClient.getDatabase("RentMe");//the cluster(project)
 
-                Document doc = new Document() ;
-                doc.append("Email", emailEditText.getText().toString());
-                doc.append("Password", passwordEditText.getText().toString());
+                    Document doc = new Document();
+                    doc.append("Email", emailEditText.getText().toString());
+                    doc.append("Password", passwordEditText.getText().toString());
 
-                mongoDatabase.getCollection("Person").insertOne(doc).getAsync(result -> {
-                    if(result.isSuccess()) {
-                        Log.v("Data", "Data Inserted Successfully");
-                        Snackbar.make(view, "Data Inserted Successfully", Snackbar.LENGTH_LONG)
-                                .setAction("Action", null).show();
-                    }
-                    else
-                        Log.v("Data","our Error " + result.getError().toString());
-                });
-
+                    mongoDatabase.getCollection("Person").insertOne(doc).getAsync(result -> {
+                        if (result.isSuccess()) {
+                            Log.v("Data", "Data Inserted Successfully");
+                            Snackbar.make(view, "Data Inserted Successfully", Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();
+                        } else
+                            Log.v("Data", "our Error " + result.getError().toString());
+                    });
+                }
 
                 //create a new activity
                 Intent intentLoadNewActivity = new Intent(LoginActivity.this, MainActivity.class);
