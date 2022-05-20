d75dae29cd323d88aa87f7d17cc0b97c8b50e35e
Asya-Dyadyuk <drasyady@gmail.com>
2022-05-21 02:59:01 +0300

    save new call to database

--- app/src/main/java/com/example/prototype/AddNewCall.java
+++ app/src/main/java/com/example/prototype/AddNewCall.java
@@ -1,5 +1,6 @@
 package com.example.prototype;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
@@ -17,29 +18,50 @@ import android.widget.Toast;
 
 
 import com.example.prototype.DBClasess.Call;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class AddNewCall extends AppCompatActivity {
     private EditText title;
-
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    Button newCallBtn;
+    EditText message;
+    String txtTitle, txtMessage, txtCurrentDate;
+    Calendar calendar = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Button newCallBtn;
-        EditText theText;
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_call);
         this.setTitle("Open New Call");
         title = (EditText) findViewById(R.id.subject);
-        theText = (EditText) findViewById(R.id.messageBody);
-
-        int duration = Toast.LENGTH_SHORT;
-
-
+        message = (EditText) findViewById(R.id.messageBody);
         newCallBtn = (Button) findViewById(R.id.submitCall);
+
+
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // Initialize Firebase Firestore
+        db = FirebaseFirestore.getInstance();
+        SignUpCall();
+        /*int duration = Toast.LENGTH_SHORT;
+
+
+
         //set what happens when the user clicks
         newCallBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,7 +108,7 @@ public class AddNewCall extends AppCompatActivity {
     /**
      * a method for the snooze
      */
-    private void createNotificationChannel() {
+   /* private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "name";
             String description = "channel";
@@ -98,6 +120,46 @@ public class AddNewCall extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
 
 
-        }
+        }*/
+
     }
+    private void SignUpCall() {
+        //progressBar.setVisibility(View.VISIBLE);
+        //btnSignUp.setVisibility(View.INVISIBLE);
+
+        newCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtTitle = title.getText().toString().trim();
+                txtMessage= message.getText().toString().trim();
+                txtCurrentDate = DateFormat.getDateInstance().format(calendar.getTime());
+                Map<String, Object> call = new HashMap<>();
+                call.put("Subject", txtTitle);
+                call.put("CallBody", txtMessage);
+                call.put("Date", txtCurrentDate);
+
+
+                db.collection("calles")
+                        .add(call)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(AddNewCall.this, "Data Stored Successfully !", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(AddNewCall.this, CallHandlingActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(AddNewCall.this, "Error - " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+
+    }
+
 }
