c2722f2c2d5c8d502c782456c26eb649e24102af
Asya-Dyadyuk <drasyady@gmail.com>
2022-07-18 20:33:22 +0300

    better logout

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.prototype;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.prototype.DBConnections.FirebaseConnection;
+import com.example.prototype.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -20,16 +25,17 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton myHistoryBtn;
     private ImageButton searchBtn;
     private String userEmail = null;
-    private Button btnLogout;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     String identity;
+    ActivityMainBinding binding; //for the menu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         //get the user email
         Bundle bundle = getIntent().getExtras();
         if (bundle.getString("userEmail") != null)
@@ -47,18 +53,6 @@ public class MainActivity extends AppCompatActivity {
         db = FirebaseConnection.getFirebaseFirestore();
         mAuth = FirebaseAuth.getInstance();
 
-        //set what happens when the user clicks "Logout"
-        btnLogout = findViewById(R.id.logout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                finish();
-                Toast.makeText(MainActivity.this, "Logout Successful", Toast.LENGTH_SHORT).show();
-            }
-        });
 
         //set what happens when the user clicks "Find Apartment"
         homeBtn.setOnClickListener(new View.OnClickListener() {
@@ -104,4 +98,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_settings,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.settings:
+                Toast.makeText(this, "Settings is clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.logout:
+                mAuth.signOut();
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                finish();
+                Toast.makeText(MainActivity.this, "Logout Successful", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
