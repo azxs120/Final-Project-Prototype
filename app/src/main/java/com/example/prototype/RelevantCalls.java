8724afb5a3be23353cb70e494642651a986ee483
Asya-Dyadyuk <drasyady@gmail.com>
2022-07-30 19:41:52 +0300

    relevant calls

--- app/src/main/java/com/example/prototype/RelevantCalls.java
+++ app/src/main/java/com/example/prototype/RelevantCalls.java
@@ -3,6 +3,7 @@ package com.example.prototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.prototype.DBClasess.Apartment;
 import com.example.prototype.DBClasess.Call;
@@ -10,7 +11,7 @@ import com.example.prototype.DBClasess.Call;
 import java.util.ArrayList;
 
 public class RelevantCalls extends AppCompatActivity {
-
+    EditText subject, messageBody, startDate, endDate, homeOwnerPhoneNumber;
     ArrayList<Call> callsList = new ArrayList<>();
 
 
@@ -18,9 +19,20 @@ public class RelevantCalls extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relevant_calls);
+        this.setTitle("Apartment History");
 
         callsList = (ArrayList<Call>) getIntent().getSerializableExtra("relevantCalls");
+        subject = (EditText) findViewById(R.id.subject);
+        messageBody = (EditText) findViewById(R.id.messageBody);
+        startDate = (EditText) findViewById(R.id.startDate);
+        endDate = (EditText) findViewById(R.id.endDate);
+        homeOwnerPhoneNumber = (EditText) findViewById(R.id.homeOwnerPhoneNumber);
 
+       // subject.setText(callsList.getSubject());
+        //messageBody.setText(callBody);
+       // startDate.setText(startDateDataMember);
+       // endDate.setText(endDteDataMember);
+       // homeOwnerPhoneNumber.setText(homeOwnerPhoneNumber);
 
 
     }
