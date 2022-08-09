6bf35c3e6f2d96d5bfd9b793e45f0ec36a507234
azxs120 <hazi120@walla.com>
2022-08-09 19:39:51 +0300

    apartmentId fix in publish apartment

--- app/src/main/java/com/example/prototype/PublishApartment.java
+++ app/src/main/java/com/example/prototype/PublishApartment.java
@@ -11,18 +11,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.prototype.DBClasess.Call;
 import com.example.prototype.DBConnections.FirebaseConnection;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.DateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PublishApartment extends AppCompatActivity {
+public class PublishApartment extends AppCompatActivity implements View.OnClickListener{
     private TextView CityText, StreetText;
     private CheckBox airConditioning, parking, balcony, elevator, bars, disabledAcess, renovated, furnished, mamad, pets;
     private Button PublishBtn;
@@ -30,7 +35,7 @@ public class PublishApartment extends AppCompatActivity {
     private String txtCity, txtStreet, txtAirConditioning, txtparking, txtbalcony, txtelevator;
     private String txtbars, txtdisabledAcess, txtrenovated, txtfurnished, txtmamad, txtpets;
     private static int apartmentId = 3;
-
+    int maxId = 0;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
 
@@ -43,6 +48,29 @@ public class PublishApartment extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         if (bundle.getString("userMobilNumber") != null)
             HomeOwnerMobilNumber = bundle.getString("userMobilNumber");
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseConnection.getFirebaseAuth();
+        db = FirebaseConnection.getFirebaseFirestore();
+
+        db.collection("apartments")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {//אם הגישה לטבלה הצליחה
+                        if (task.isSuccessful()) {
+
+                            for (QueryDocumentSnapshot doc : task.getResult()) {//תביא את כל הרשומות
+                                String apartmentId = doc.getString("Apartment Id");
+                                if(Integer.parseInt(apartmentId) > maxId)
+                                    maxId = Integer.parseInt(apartmentId);
+                            }
+                        }
+                    }
+                });
+
+
+
 
         CityText = (EditText) findViewById(R.id.CityText);
         StreetText = (EditText) findViewById(R.id.StreetText);
@@ -58,99 +86,95 @@ public class PublishApartment extends AppCompatActivity {
         mamad = (CheckBox) findViewById(R.id.mamad);
         PublishBtn = (Button) findViewById(R.id.PublishBtn);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseConnection.getFirebaseAuth();
-        db = FirebaseConnection.getFirebaseFirestore();
-
-        PublishBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txtCity = CityText.getText().toString().trim();
-                txtStreet = StreetText.getText().toString().trim();
-
-                if (airConditioning.isChecked())
-                    txtAirConditioning = "true";
-                else
-                    txtAirConditioning = "false";
-                if (parking.isChecked())
-                    txtparking = "true";
-                else
-                    txtparking = "false";
-                if (balcony.isChecked())
-                    txtbalcony = "true";
-                else
-                    txtbalcony = "false";
-                if (elevator.isChecked())
-                    txtelevator = "true";
-                else
-                    txtelevator = "false";
-                if (bars.isChecked())
-                    txtbars = "true";
-                else
-                    txtbars = "false";
-                if (disabledAcess.isChecked())
-                    txtdisabledAcess = "true";
-                else
-                    txtdisabledAcess = "false";
-                if (renovated.isChecked())
-                    txtrenovated = "true";
-                else
-                    txtrenovated = "false";
-                if (furnished.isChecked())
-                    txtfurnished = "true";
-                else
-                    txtfurnished = "false";
-                if (mamad.isChecked())
-                    txtmamad = "true";
-                else
-                    txtmamad = "false";
-                if (pets.isChecked())
-                    txtpets = "true";
-                else
-                    txtpets = "false";
-
-                Map<String, Object> apartment = new HashMap<>();
-                apartment.put("City", txtCity);
-                apartment.put("Street", txtStreet);
-                apartment.put("Air Conditioning", txtAirConditioning);
-                apartment.put("Parking", txtparking);
-                apartment.put("Balcony", txtbalcony);
-                apartment.put("Elevator", txtelevator);
-                apartment.put("Bars", txtbars);
-                apartment.put("Disabled Acess", txtdisabledAcess);
-                apartment.put("Renovated", txtrenovated);
-                apartment.put("Furnished", txtfurnished);
-                apartment.put("Panic Room", txtmamad);
-                apartment.put("Pets", txtpets);
-                apartment.put("Home Owner Mobile Number", HomeOwnerMobilNumber);
-                apartment.put("Apartment Id", apartmentId);
-                apartmentId ++;
-
-
-                db.collection("apartments")
-                        .add(apartment)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-
-                                Toast.makeText(PublishApartment.this, "The apartment was successfully advertised!", Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-
-
-                                Toast.makeText(PublishApartment.this, "----- Error ----- the call was not created", Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                        });
-
-            }
-
-        });
-
+        PublishBtn.setOnClickListener(this);
 
     }
+
+        @Override
+        public void onClick(View v) {
+
+            txtCity = CityText.getText().toString().trim();
+            txtStreet = StreetText.getText().toString().trim();
+
+            if (airConditioning.isChecked())
+                txtAirConditioning = "true";
+            else
+                txtAirConditioning = "false";
+            if (parking.isChecked())
+                txtparking = "true";
+            else
+                txtparking = "false";
+            if (balcony.isChecked())
+                txtbalcony = "true";
+            else
+                txtbalcony = "false";
+            if (elevator.isChecked())
+                txtelevator = "true";
+            else
+                txtelevator = "false";
+            if (bars.isChecked())
+                txtbars = "true";
+            else
+                txtbars = "false";
+            if (disabledAcess.isChecked())
+                txtdisabledAcess = "true";
+            else
+                txtdisabledAcess = "false";
+            if (renovated.isChecked())
+                txtrenovated = "true";
+            else
+                txtrenovated = "false";
+            if (furnished.isChecked())
+                txtfurnished = "true";
+            else
+                txtfurnished = "false";
+            if (mamad.isChecked())
+                txtmamad = "true";
+            else
+                txtmamad = "false";
+            if (pets.isChecked())
+                txtpets = "true";
+            else
+                txtpets = "false";
+
+            apartmentId = maxId + 1;
+
+            Map<String, Object> apartment = new HashMap<>();
+            apartment.put("City", txtCity);
+            apartment.put("Street", txtStreet);
+            apartment.put("Air Conditioning", txtAirConditioning);
+            apartment.put("Parking", txtparking);
+            apartment.put("Balcony", txtbalcony);
+            apartment.put("Elevator", txtelevator);
+            apartment.put("Bars", txtbars);
+            apartment.put("Disabled Acess", txtdisabledAcess);
+            apartment.put("Renovated", txtrenovated);
+            apartment.put("Furnished", txtfurnished);
+            apartment.put("Panic Room", txtmamad);
+            apartment.put("Pets", txtpets);
+            apartment.put("Home Owner Mobile Number", HomeOwnerMobilNumber);
+            apartment.put("Apartment Id", String.valueOf(apartmentId));
+
+            db.collection("apartments")
+                    .add(apartment)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+
+                            Toast.makeText(PublishApartment.this, "The apartment was successfully advertised!", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+
+                            Toast.makeText(PublishApartment.this, "----- Error ----- the call was not created", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+
+        }
+
 }
